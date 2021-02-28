3989af1946e892d51bf897bbb99526add8d3049f
quesrbt <59384971+quesrbt@users.noreply.github.com>
2021-02-28 14:39:25 -0800

    Start grades page

---
+++ frontend/src/app/pages/grades/grades.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './grades.component.html',
+  styleUrls: ['./grades.component.scss']
+})
+export class GradesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
