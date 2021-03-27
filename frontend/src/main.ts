71df33776f250c8d54aea04a9036356ec4a643c0
quesrbt <59384971+quesrbt@users.noreply.github.com>
2021-03-27 10:46:21 -0700

    Add google authentication button

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,3 +10,11 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}  
